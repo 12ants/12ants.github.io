546295d956da6ebe7d36ea32dd3ba4579504ff50
Leon Ljunghorn <leonljunghorn@gmail.com>
2022-12-16 18:48:23 +0100

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -135,7 +135,8 @@ cd /boot/grub/ ;
 ####################################
 #### -Download Picture--############
 ####################################
-wget https://0smik.github.io/tard4.jpg ;
+wget https://github.com/0smik/ss/raw/main/tard.jpg ;
+wget https://github.com/0smik/ss/raw/main/tard4.jpg ;
 ####################################
 #### -Write GRUB config file--######
 ####################################
@@ -147,16 +148,16 @@ GRUB_TIMEOUT_STYLE=menu
 GRUB_TIMEOUT=12
 GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
 GRUB_DISABLE_OS_PROBER=false
-GRUB_GFXMODE="640x480"
+GRUB_GFXMODE="800x600"
 GRUB_HIDDEN_TIMEOUT=0
 ' > /etc/default/grub ; 
-sleep 0.2 ; 
+sleep 0.4 ; 
 update-grub ; 
 echo "GRUB INSTALLED SUCCESSFULLY"
+fi
 ####################################
 #### -All done!--###################
 ####################################
-fi
 
 
 
