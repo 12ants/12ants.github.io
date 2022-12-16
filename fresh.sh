e6cd9ea0b9cfc3e2233dfd962cd932769b354834
Leon Ljunghorn <leonljunghorn@gmail.com>
2022-12-16 19:42:44 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -4,7 +4,7 @@
 ####
 apt update
 apt install gpm -y                 #  bash mouse support
-apt install -y -qq xfce4-session xfce4-goodies xfce4-panel xinit dillo      #  minimal desktop env
+apt install -y -qq xfce4-session xfce4-goodies xfce4-panel synaptic xinit luakit     #  minimal desktop env
 # snap install firefox
 ####
 startx # to start xfce4 (might need reboot)
