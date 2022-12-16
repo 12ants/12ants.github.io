dcc178dfb22a199eae92488165423d87dc3a4ced
Leon Ljunghorn <leonljunghorn@gmail.com>
2022-12-16 12:24:39 +0100

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -135,20 +135,20 @@ cd /boot/grub/ ;
 ####################################
 #### -Download Picture--############
 ####################################
-wget https://github.com/0smik/ss/raw/main/tard4.jpg ;
+wget https://0smik.github.io/tard4.jpg ;
 ####################################
 #### -Write GRUB config file--######
 ####################################
 echo '
 GRUB_BACKGROUND="/boot/grub/tard4.jpg"
 GRUB_DEFAULT=saved
-GRUB_SAVEDEFAULT=false
+GRUB_SAVEDEFAULT=true
 GRUB_TIMEOUT_STYLE=menu
 GRUB_TIMEOUT=12
-GRUB_DISTRIBUTOR=`osmik.se ; lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_DISTRIBUTOR=`echo osmik.se ; lsb_release -i -s 2> /dev/null || echo Debian`
 GRUB_DISABLE_OS_PROBER=false
-GRUB_GFXMODE="800x600"
-GRUB_HIDDEN_TIMEOUT=1
+GRUB_GFXMODE="64x480"
+GRUB_HIDDEN_TIMEOUT=0
 ' > /etc/default/grub ; 
 sleep 0.2 ; 
 update-grub ; 
@@ -208,12 +208,12 @@ fi
 
 
 ###################################
-#### WEBMIN--Install ##############
+#### WP--Install ##############
 ###################################
 ####
 echo ${cyan};echo "${c75}";echo "${c75}";echo "${c75}";echo "${c75}";echo "${c75}";echo "${c75}";echo "${c75}";
 tput cuu 4;
-read -p ${cyan}"  ----------${re} Install WEBMIN? [Y/n]   ${left2}" yn;
+read -p ${cyan}"  ----------${re} Install Wordpress? [Y/n]   ${left2}" yn;
 tput cuf 52 cuu 1;
 if [ "$yn" != "${yn#[Nn]}" ];
 then 
