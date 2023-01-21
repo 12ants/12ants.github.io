a9fbb25fecff6e4f415ff4146113608fc2698c2d
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-01-21 23:24:16 +0100

    Create logins-msg.sh

---
+++ logins-msg.sh
@@ -0,0 +1,21 @@
+
+
+## console login default
+sudo systemctl set-default multi-user.target
+## gui def
+## sudo systemctl set-default graphical.target
+
+
+
+## Message before login
+echo "Hello login" > /etc/issue /etc/ssh/sshd-banner
+echo "Banner /etc/ssh/sshd-banner" > /etc/ssh/sshd-config
+echo "Welcome to 12ants -- \d \t @ \n" >> /etc/issue
+/etc/init.d/ssh restart
+
+
+## Message after login
+echo "fortune | cowsay -pn" >> ~.bashrc 
+echo "fortune | cowsay -pn" >> /etc/motd 
+
+ssh restart
