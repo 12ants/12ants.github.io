c90c02f27f593c589bb90e4922a1629245273ebe
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-01-21 22:20:59 +0100

    Update grub.sh

--- grub.sh
+++ grub.sh
@@ -1,4 +1,5 @@
 
+
 ####################################
 #### INSTALL CUSTOM GRUB ###########
 ####################################
@@ -20,27 +21,33 @@ cd /boot/grub/ ;
 ####################################
 #### -Download Picture--############
 ####################################
-wget https://github.com/0smik/ss/raw/main/tard.jpg ;
-wget https://github.com/0smik/ss/raw/main/tard4.jpg ;
+wget https://github.com/12ants/ss/raw/main/tard.jpg ;
 ####################################
 #### -Write GRUB config file--######
 ####################################
+rm /etc/default/grub.d/*;
+
 echo '
-GRUB_BACKGROUND="/boot/grub/tard4.jpg"
+GRUB_BACKGROUND="/boot/grub/tard.jpg"
 GRUB_DEFAULT=saved
 GRUB_SAVEDEFAULT=true
 GRUB_TIMEOUT_STYLE=menu
 GRUB_TIMEOUT=12
 GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
 GRUB_DISABLE_OS_PROBER=false
-GRUB_GFXMODE="800x600"
+GRUB_GFXMODE="1024x768"
 GRUB_HIDDEN_TIMEOUT=0
-' > /etc/default/grub ; 
-sleep 0.4 ; 
+' > /etc/default/grub; 
+
+ 
+
+
+sleep 0.4; 
 update-grub ; 
-echo "GRUB INSTALLED SUCCESSFULLY"
+sleep 0.4; echo -e "\v\t -- GRUB INSTALLED SUCCESSFULLY \v" sleep 0.4; 
 fi
 ####################################
 #### -All done!--###################
 ####################################
+
 
