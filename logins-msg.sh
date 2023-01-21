96790a86498cac34a846ef827304cc21596f825b
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-01-21 23:41:30 +0100

    Update logins-msg.sh

--- logins-msg.sh
+++ logins-msg.sh
@@ -1,24 +1,28 @@
 
 
+
+
 ## console login default
 sudo systemctl set-default multi-user.target
 ## gui def
 ## sudo systemctl set-default graphical.target
 
-
-
 ## Message before login
 
 ## echo "Hello login" > /etc/issue /etc/ssh/sshd-banner
 ## echo "Banner /etc/ssh/sshd-banner" > /etc/ssh/sshd-config
-echo "Welcome to 12ants.com -- today is \d \t @ \n" >> /etc/issue
-/etc/init.d/ssh restart
 
+apt install -y ssh fortune cowsay
+
+echo "Welcome to 12ants.com -- today is \d \t @ \n" >> /etc/issue
+
+/etc/init.d/ssh restart 
 
 ## Message after login
 ## echo "fortune | cowsay -pn" >> ~.bashrc 
 echo "fortune | cowsay -pn
+" >> /etc/profile 
 
-" >> /etc/motd 
+echo "hello" > /etc/motd 
 
 ssh restart
