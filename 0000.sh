b6cb3810d342acc20db55dc0f2b1ded3aa7f2e6c
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-02-17 22:42:54 +0100

    Update 0000.sh

--- 0000.sh
+++ 0000.sh
@@ -49,6 +49,54 @@ wget -O 12grub $rootgit/grub.sh; bash 12grub;
 else echo "no"; fi;
 
 
+###################################
+#### DISABLE ROOT PASSWORD ########
+#### ADD AUTOCOMPLETE #############
+#### COLOR-ALIAS ##################
+###################################
+###################################
+
+if [ "$auto" == "y" ]; then
+###### ---- [YES] ----- ######
+########### DISABLE PWD ###########
+echo "${re} OK ";sleep 2;echo ${dim} "${dim}" ${noso}; echo -e "\v\v\v\v";
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-installer;
+############ ROOT AUTOCOMPLETE
+echo "
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then 
+    . /etc/bash_completion
+fi
+" >> /root/.bashrc;
+## COLOR-ALIAS
+echo "PS1='\[\e[34m\]\u\[\e[36m\]@\[\e[2m\]\h\[\e[34m\]\[\e[96m\]\w:\[\e[m\]'
+###############################################
+#### DISPLAY IP - NET & PUB (TO THE RIGHT) ####
+## NETWORK-IP
+netip="$(hostname -I)"
+## PUBLIC-IP
+pubip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+function prompt_command {
+#  prompt_x is where to position the cursor to write the clock
+let prompt_x=$(tput cols)-20
+#  Move up one; not sure why we need to do this, but without this, I always
+tput sc; tput cup 0 ${prompt_x}; tput setaf 4;
+echo -e "                                ";
+tput cup 1 ${prompt_x}; echo -e "${netip} / ${pubip}"; tput rc
+}
+PROMPT_COMMAND=prompt_command
+###############################################
+
+" >> ~/.bash_aliases;
+echo "${re}${green}${bold}     DONE ${re}";echo;echo;echo;echo;
+else echo no
+fi
+fi 
+
+
+
+
+
+
 
 
 echo "$green $blink
