fcb7e885336eeab4de66f4ba89d94dcd96c29731
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-02-22 18:28:18 +0100

    Update 0000.sh

--- 0000.sh
+++ 0000.sh
@@ -10,9 +10,8 @@ redb=$(tput setab 1) greenb=$(tput setab 2) yellowb=$(tput setab 3) blueb=$(tput
 grayb=$(tput setab 7) red=$(tput setaf 1) green=$(tput setaf 2) yellow=$(tput setaf 3) blue=$(tput setaf 4) purple=$(tput setaf 5) \
 cyan=$(tput setaf 6) gray=$(tput setaf 7) white=$(tput setaf 7 bold) pink=$(tput setaf 5 bold) darkblue=$(tput setab 5 bold) blink=$(tput blink) \
 left2=$(tput cub 2) up1=$(tput cuu1) c75="  ---------------------------------------------------------------------------"; clear; echo ; c2="$cyan --$re";
-
-
-mkdir -p /home/00/; chmod 775 /home/00; cd /home/00/; clear; echo ;
+## WELCOME-SCREEN
+inst="/home/00/"; mkdir -p $inst; chmod 775 $inst; cd $inst; clear; echo ;
 echo -e "
   $c2  Welcome to$pink Linux-tweaks$re by$green 12ants.com$re
   $c2  Please choose preferred actions \n \n ";
@@ -29,9 +28,14 @@ read -ep "  $c2  install$green hestia-web-server? $re  [y/n]: " -i "n" "hestia"
 read -ep "  $c2  install$green guake? $re              [y/n]: " -i "n" "guake"
 read -ep "  $c2  install$green custom-grub? $re        [y/n]: " -i "n" "grub"
 read -ep "  $c2  install$green sudo-color? $re         [y/n]: " -i "n" "auto"
+read -ep "  $c2  install$green 4xfce GUI-OS? $re       [y/n]: " -i "n" "xfce"
+read -ep "  $c2  install$green login-screen? $re       [y/n]: " -i "n" "login"
+read -ep "  $c2  install$green webmin? $re             [y/n]: " -i "n" "webmin"
+
+
 read -ep "$purple ---------------------------------------------$re
 
-$c2 $yellow begin installation? $re         [y/n]: " -i "y"               "continue"
+$c2 $yellow begin installation? $re         [y/n]: " -i "n"               "continue"
   
 if [ $continue == y ]; then echo -e "\n\n\t --$cyan OK$re -- \n\n"; else exit 1; fi; 
 ## REMEMER TO CHANGE VAR-NAMES.
@@ -42,36 +46,62 @@ apt -y install curl wget sudo;
 curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh; \
 echo "f25e3fe3dc028ef8eda281868ab606b5b80bc6ba74a253ae54ab5fd1e61c287d install.sh" | \
 sha256sum -c && sudo bash install.sh
-else echo "OK"; fi
+else echo "OK"; fi; cd $inst;
 
 ## 
 if [ $hestia == y ]; then echo "installing hestia";
-wget -O 12hestia https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh; bash 12hestia;
-else echo "OK"; fi
+wget -O 12hestia.sh https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh; bash 12hestia.sh;
+else echo "OK"; fi cd $inst;
 
 ##
 if [ $guake == y ]; then echo "installing guake";
 apt -y install guake;
-else echo "OK"; fi
+else echo "OK"; fi cd $inst;
 
 ##
 if [ $grub == y ]; then echo "installing grub";
 wget -O 12grub.sh $rootgit/sh/grub.sh; bash 12grub.sh;
-else echo "OK"; fi
+else echo "OK"; fi cd $inst;
 
 ##
 if [ $auto == y ]; then tput blink ; echo "installing auto-sudo";
 wget -O auto-sudo.sh $rootgit/sh/auto-sudo.sh; bash auto-sudo.sh; 
-else echo "OK"; fi
- 
+else echo "OK"; fi cd $inst;
+  
+
+if [ $xfce == y ]; then tput blink ; echo "installing xfce";
+wget https://12ants.github.io/root.tar; clear; echo -e "\n\t -- This might take a while \n\t "
+apt install -y -qq xfce4-session xfce4-goodies xfce4-panel synaptic xinit luakit firefox guake    #  minimal desktop env
+echo -e "\v\t Type [ startx ] to execute \v\v"
+else echo "OK"; fi cd $inst;
 
 
+##
+if [ $login == y ]; then echo "installing login-screen";
+wget -O login.sh $rootgit/sh/login.sh; bash login.sh;
+else echo "OK"; fi cd $inst;
+
+##
+##
+if [ $webmin == y ]; then echo "installing webmin";
+wget https://download.webmin.com/devel/tarballs/webmin-current.tar.gz;
+tar -xf webmin-current.tar.gz --strip-components=1; 
+./setup.sh /usr/local/webmin;
+else echo "OK"; fi cd $inst;
 
 
-echo "
+echo -e "
   $c2 -- $green $blink https://12ants.github.io
 $re $normal
 
+$(hostname -I) 
   enjoy!
 
 "
+
+read -t 5 -ep "  -- Restarting in 5 seconds: " -i Abort "restarts"
+if [ $restarts == Abort ] then echo ok
+else reboot;
+fi
+echo gg
+
