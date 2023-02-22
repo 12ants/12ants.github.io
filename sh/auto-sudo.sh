5a0e79186cc13c8b968385c20a977e7e54458503
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-02-22 17:58:51 +0100

    Update auto-sudo.sh

--- sh/auto-sudo.sh
+++ sh/auto-sudo.sh
@@ -8,21 +8,17 @@ fi
 ## COLOR-ALIAS
 echo "PS1='\[\e[34m\]\u\[\e[36m\]@\[\e[2m\]\h\[\e[34m\]\[\e[96m\]\w:\[\e[m\]'
 ###############################################
+
 #### DISPLAY IP - NET & PUB (TO THE RIGHT) ####
+
 ## NETWORK-IP
+
 netip="$(hostname -I)"
 ## PUBLIC-IP
 pubip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-function prompt_command {
-#  prompt_x is where to position the cursor to write the clock
-let prompt_x=$(tput cols)-40
-#  Move up one; not sure why we need to do this, but without this, I always
-tput sc; tput cup 0 ${prompt_x}; tput setaf 4;
-echo -e "                                ";
-tput cup 0; ${prompt_x} 
-echo -e "${netip} / ${pubip}"; tput rc
-}
-PROMPT_COMMAND=prompt_command
+
+function b_alias { tput sc; tput cup 0 0; tput setaf 6; echo -e "${netip} / ${pubip}"; tput rc; } ; PROMPT_COMMAND=b_alias;
+
 ###############################################
 
 " >> ~/.bash_aliases;
