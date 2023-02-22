d462aeca35a8faf2ad4a50d66fd92fbaa12c8f09
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-02-22 14:14:34 +0100

    Update 0000.sh

--- 0000.sh
+++ 0000.sh
@@ -3,10 +3,7 @@
 ## visit https://12ants.github.io for credits
 rootgit=12ants.github.io;
 if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; exit 1; fi; ## ROOT-CHECK
-mkdir -p /home/00/; chmod 775 /home/00 cp /home/00/; clear; echo ; echo ; 
-read -ep "  --  Root repo for install-files: [Press Enter to continue] " -i "${rootgit}" rootgit;
-read -ep "  --  update system? [y/n]: " -i "n" "upsys";
-if [ $upsys == y ]; then echo "updating..."; apt update; apt -y upgrade; clear; echo ""; echo ""; else echo ok ; fi; 
+
 ## ADDING COLOR-CODES -- (Need to run inside other command.)
 export bold=$(tput bold) dim=$(tput dim) so=$(tput smso) noso=$(tput rmso) rev=$(tput rev) re=$(tput sgr0) normal=$(tput sgr0) \
 redb=$(tput setab 1) greenb=$(tput setab 2) yellowb=$(tput setab 3) blueb=$(tput setab 4) purpleb=$(tput setab 5) cyanb=$(tput setab 6) \
@@ -14,74 +11,60 @@ grayb=$(tput setab 7) red=$(tput setaf 1) green=$(tput setaf 2) yellow=$(tput se
 cyan=$(tput setaf 6) gray=$(tput setaf 7) white=$(tput setaf 7 bold) pink=$(tput setaf 5 bold) darkblue=$(tput setab 5 bold) blink=$(tput blink) \
 left2=$(tput cub 2) up1=$(tput cuu1) c75="  ---------------------------------------------------------------------------"; clear; echo ; c2="$cyan --$re";
 
+
+mkdir -p /home/00/; chmod 775 /home/00 cd /home/00/; clear; echo ;
+echo -e "
+  $c2  Welcome to$pink Linux-tweaks$re by$green 12ants.com$re
+  $c2  Please choose preferred actions \n \n ";
+read -ep "  $c2  Root repo for install-files: [Press Enter to continue] " -i "${rootgit}/" rootgit;
+read -ep "  $c2  update system? [y/n]: " -i "n" "upsys";
+if [ $upsys == y ]; then echo "updating..."; apt update; apt -y upgrade; clear; echo ""; echo ""; else echo ok ; fi; 
+clear;
 ##############################
-######## INSTALLER ############
+######## INSTALLER ###########
 ######################
-echo -e "\n\n\t --$pink Software installation$re -- \n\n"
+echo -e "\n\n\t $c2 $pink Software installation$re -- \n\n"
 read -ep "  $c2  install$green cloudpanel? $re         [y/n]: " -i "n" "cloudpanel"
 read -ep "  $c2  install$green hestia-web-server? $re  [y/n]: " -i "n" "hestia"
 read -ep "  $c2  install$green guake? $re              [y/n]: " -i "n" "guake"
 read -ep "  $c2  install$green custom-grub? $re        [y/n]: " -i "n" "grub"
 read -ep "  $c2  install$green sudo-color? $re         [y/n]: " -i "n" "auto"
 read -ep "$purple ---------------------------------------------$re
-  $c2 $yellow begin installation? $re         [y/n]: " -i "n"               "continue"
+  $c2 $yellow begin installation? $re         [y/n]: " -i "y"               "continue"
   
   
   
 if [ $continue == y ]; then echo -e "\n\n\t --$cyan OK$re -- \n\n"; else exit 1; fi; 
-##
 ## REMEMER TO CHANGE VAR-NAMES.
+
+##
 if [ $cloudpanel == y ]; then echo "installing cloudpanel";
-apt update && apt -y upgrade && apt -y install curl wget sudo
+apt -y install curl wget sudo;
 curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh; \
 echo "f25e3fe3dc028ef8eda281868ab606b5b80bc6ba74a253ae54ab5fd1e61c287d install.sh" | \
 sha256sum -c && sudo bash install.sh
-else echo "no"; fi
+else echo "OK"; fi;
+
 ## 
 if [ $hestia == y ]; then echo "installing hestia";
 wget -O 12hestia https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install.sh; bash 12hestia;
-else echo "no"; fi;
+else echo "OK"; fi;
+
 ##
 if [ $guake == y ]; then echo "installing guake";
 apt -y install guake;
-else echo "no"; fi;
+else echo "OK"; fi;
+
 ##
 if [ $grub == y ]; then echo "installing grub";
-wget -O 12grub $rootgit/grub.sh; bash 12grub;
-else echo "no"; fi;
-##
-if [ "$auto" == "y" ]; then
-echo "%sudo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-installer;
-############ ROOT AUTOCOMPLETE
-echo "
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then 
-    . /etc/bash_completion
-fi
-" >> /root/.bashrc;
-## COLOR-ALIAS
-echo "PS1='\[\e[34m\]\u\[\e[36m\]@\[\e[2m\]\h\[\e[34m\]\[\e[96m\]\w:\[\e[m\]'
-###############################################
-#### DISPLAY IP - NET & PUB (TO THE RIGHT) ####
-## NETWORK-IP
-netip="$(hostname -I)"
-## PUBLIC-IP
-pubip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-function prompt_command {
-#  prompt_x is where to position the cursor to write the clock
-let prompt_x=$(tput cols)-40
-#  Move up one; not sure why we need to do this, but without this, I always
-tput sc; tput cup 0 ${prompt_x}; tput setaf 4;
-echo -e "                                ";
-tput cup 0; ${prompt_x}; echo -e "${netip} / ${pubip}"; tput rc
-}
-PROMPT_COMMAND=prompt_command
-###############################################
+wget -O 12grub.sh $rootgit/grub.sh; bash 12grub.sh;
+else echo "OK"; fi;
 
-" >> ~/.bash_aliases;
-echo "${re}${green}${bold}     DONE ${re}";echo;echo;echo;echo;
-else echo no
-fi
-fi 
+##
+if [ $auto == y ]; then tput blink ; echo "installing auto-sudo";
+wget -O auto-sudo.sh $rootgit/auto-sudo.sh; bash auto-sudo.sh; 
+else echo "OK"; fi;
+ 
 
 
 
