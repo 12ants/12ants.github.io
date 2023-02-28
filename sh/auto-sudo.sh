51c565ebe3becbf8b6c6747ee8247ec8891cfb49
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-02-28 04:08:00 +0100

    Update auto-sudo.sh

--- sh/auto-sudo.sh
+++ sh/auto-sudo.sh
@@ -7,19 +7,21 @@ echo "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then ; . /etc/bash_co
 
 
 ## COLOR-ALIAS
-echo "PS1='\[\e[34m\]\u\[\e[36m\]@\[\e[2m\]\h\[\e[34m\]\[\e[96m\]\w:\[\e[m\]'
 ###############################################
 
 #### DISPLAY IP - NET & PUB (TO THE RIGHT) ####
 
 ## NETWORK-IP
 
-netip="$(hostname -I)"
+## netip="$(hostname -I)"
 ## PUBLIC-IP
-pubip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+## pubip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-function b_alias { tput sc; tput cup 0 0; tput setaf 5; echo -e "$netip / $pubip"; tput rc; } ; PROMPT_COMMAND=b_alias;
+## function b_alias { tput sc; tput cup 0 0; tput setaf 5; echo -e "$netip / $pubip"; tput rc; } ; PROMPT_COMMAND=b_alias;
 
 ###############################################
+echo "PS1='\[\e[34m\]\u\[\e[36m\]@\[\e[96m\]$(dig +short myip.opendns.com @resolver1.opendns.com)\[\e[34m\]/\e\e[36m\]$(hostname -I)\[\e[34m\]/\e[96m\]\h\e[34m\]\[\e[96m\]\w:\[\e[m\]'"
+>> ~/.bash_aliases;
 
-" >> ~/.bash_aliases;
+
+
