67e1a2a3efd1201bd77b7ca832f7056faee5b153
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-02-28 02:55:13 +0100

    Update auto-sudo.sh

--- sh/auto-sudo.sh
+++ sh/auto-sudo.sh
@@ -1,10 +1,11 @@
+## auto root for for admins
 echo "%sudo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-installer;
-############ ROOT AUTOCOMPLETE
-echo "
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then 
-    . /etc/bash_completion
-fi
-" >> /root/.bashrc;
+
+
+## ROOT AUTOCOMPLETE
+echo "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then ; . /etc/bash_completion; fi; " >> /root/.bashrc;
+
+
 ## COLOR-ALIAS
 echo "PS1='\[\e[34m\]\u\[\e[36m\]@\[\e[2m\]\h\[\e[34m\]\[\e[96m\]\w:\[\e[m\]'
 ###############################################
@@ -17,7 +18,7 @@ netip="$(hostname -I)"
 ## PUBLIC-IP
 pubip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-function b_alias { tput sc; tput cup 0 0; tput setaf 5; echo -e "${netip} / ${pubip}"; tput rc; } ; PROMPT_COMMAND=b_alias;
+function b_alias { tput sc; tput cup 0 0; tput setaf 5; echo -e "$netip / $pubip"; tput rc; } ; PROMPT_COMMAND=b_alias;
 
 ###############################################
 
